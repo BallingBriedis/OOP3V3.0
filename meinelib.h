7d81441870a05ec685c159ba3b45082a82531546
BallingBriedis <dominykas.pronskus@mif.stud.vu.lt>
2025-02-07 12:46:26 +0200

    Pradinės užduoties header failas
    Įrašyta iostream, iomanip, string, vector bibliotekos.
    Aprašyta struktūra studento vardui, pavardei, namų darbų ir egzamino pazymiams sudėlioti.

---
+++ meinelib.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct Stud{
+string var;
+string pav;
+vector<int>uzd{};
+int egz;
+};
