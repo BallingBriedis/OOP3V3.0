39facc1cac868dff57f3610253dec9a96711a32f
BallingBriedis <dominykas.pronskus@mif.stud.vu.lt>
2025-04-26 03:15:05 +0300

    Furthering Class, functionCalls, Stud.cpp addons

--- VECTOR/Include/classV.h
+++ VECTOR/Include/classV.h
@@ -15,23 +15,23 @@ private:
 	int gal;
 
 public:
-	Stud() : egz(0), gal(0) {};
-	Stud(std::string v, std::string p, std::vector<int> u, int e, int g) : var(v), pav(p), uzd(u), egz(e), gal(g) {}
+	Stud() : egz(0), gal(0) {}
 
 	void setVar(const std::string& v) {var = v;}
-	void setPav(const std::string& p) { pav = p; }
-	void setUzd(const std::vector<int>& u) { uzd = u; }
-	void setEgz(int e) { egz = e; }
-	void setGal(int g) { gal = g; }
+	void setPav(const std::string& p) {pav = p;}
+	void addUzd(int grade) {uzd.push_back(grade);}
+	void setEgz(int e) {egz = e;}
+	void setGal(int g) {gal = g;}
+	void print() const;
 
 	std::string getVar() const { return var; }
 	std::string getPav() const { return pav; }
 	std::vector<int> getUzd() const { return uzd; }
-	int getEgz() const { return egz; }
-	int getGal() const { return gal; }
+	int getEgz() const {return egz;}
+	int getGal() const {return gal;}
 
 	~Stud() {
 		std::cout << "Destruktorius\n";
-	};
+	}
 };
 #endif
--- VECTOR/src/Stud.cpp
+++ VECTOR/src/Stud.cpp
@@ -2,16 +2,31 @@
 #include "classV.h"
 #include "functionsCallsVector.h"
 
-bool compareByName(const Stud&, const Stud&) {																			// Rusiavimo funkcijos dalis kuri tikrina vardus.
+bool compareByName(const Stud& a, const Stud& b) {																			// Rusiavimo funkcijos dalis kuri tikrina vardus.
 	return a.getVar() < b.getVar();
 }
 
-bool compareBySurname(const Stud&, const Stud&) {																		// Rusiavimo funkcijos dalis kuri tikrina pavardes.
+bool compareBySurname(const Stud& a, const Stud& b) {																		// Rusiavimo funkcijos dalis kuri tikrina pavardes.
 	return a.getPav() < b.getPav();
 }
 
-bool compareByFinalGrade(const Stud&, const Stud&) {																	// Rusiavimo funkcijos dalis kuri tikrina galutinius pazymi.
+bool compareByFinalGrade(const Stud& a, const Stud& b) {																	// Rusiavimo funkcijos dalis kuri tikrina galutinius pazymi.
 	return a.getGal() < b.getGal();
+}
+
+void Stud::addUzd(int grade) {
+	uzd.push_back(grade);
+}
+
+void Stud::print() const {
+	cout << std::left << "\nVardas: " << std::setw(20) << var << "\nPavarde: " << std::setw(20) << pav << endl;
+	cout << "Namų darbai: ";
+	for (int i = 0; i < uzd.size(); i++) {
+		cout << std::setw(3) << uzd[i];
+	}
+	cout << "\n";
+	cout << "Egzaminas: " << std::setw(6) << egz << "\n";
+	cout << "Galutinis: " << std::setw(6) << gal << "\n\n";
 }
 
 // konstruktoriaus realizacija
