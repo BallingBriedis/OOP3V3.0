190c7f820501d1344b11b7a12bf120d8de926b43
BallingBriedis <dominykas.pronskus@mif.stud.vu.lt>
2025-05-31 01:20:23 +0300

    Vector class progress
    Constructors, Destructor, PushBack, Empty, Size, Capacity, ==, !=

--- VECTOR/Include/vector.h
+++ VECTOR/Include/vector.h
@@ -3,17 +3,25 @@
 
 class Vector {
 private:
-	int Size;
-	int Capacity;
-	int* Array;
+	int size;
+	int capacity;
+	int* array;
 public:
 	Vector();
+	Vector(const Vector& rhs);
+	Vector(int elements, int value = 0);
+
 	~Vector();
+
+
 	void PushBack(int value);
 
-	bool Empty();
-	int Size();
-	int Capacity();
+	bool Empty() const;
+	int Size() const;
+	int Capacity() const;
+
+	bool operator==(const Vector& rhs) const;
+	bool operator!=(const Vector& rhs) const;
 };
 
 #endif
--- VECTOR/src/Vector.cpp
+++ VECTOR/src/Vector.cpp
@@ -2,18 +2,68 @@
 
 
 Vector::Vector() :
-	Size(0),
-	Capacity(5),
-	Array(new int[Capacity]) {}
+	size(0),
+	capacity(5),
+	array(new int[capacity]) {}
+
+Vector::Vector(const Vector& rhs) :
+	size(rhs.size),
+	capacity(rhs.capacity),
+	array(new int[capacity])
+{
+	for (int i = 0; i < rhs.Size(); ++i) {
+		array[i] = rhs.array[i];
+	}
+}
+
+Vector::Vector(int elements, int value) :
+	size(elements),
+	capacity(elements + 5),
+	array(new int[capacity])
+{
+	for (int i = 0; i < size; ++i) {
+		array[i] = value;
+	}
+}
 
 Vector::~Vector(){
-	delete[] Array;
-	Size = 0;
-	Capacity = 0;
-	Array = nullptr;
+	delete[] array;
+	size = 0;
+	capacity = 0;
+	array = nullptr;
 }
 
 void Vector::PushBack(int value) {
-	Array[Size] = value;
-	++Size;
+	array[size] = value;
+	++size;
+}
+
+bool Vector::Empty() const {
+	return size == 0;;
+}
+
+int Vector::Size() const {
+	return size;
+}
+
+int Vector::Capacity() const {
+	return capacity;
+}
+
+bool Vector::operator==(const Vector& rhs) const {
+	if (Size() != rhs.Size()) {
+		return false;
+	}
+
+	for (int i = 0; i < Size(); ++i) {
+		if (array[i] != rhs.array[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Vector::operator!=(const Vector& rhs) const
+{
+	return !(*this == rhs);
 }
